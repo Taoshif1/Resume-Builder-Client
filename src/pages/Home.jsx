defeb6107832cb067483c4d6c7091d39d65e9db5
sanjidasefa <sanjidasefa9@gmail.com>
2026-04-11 22:58:16 +0600

    add assistant modes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import PricingDemo from "./HomePage/PricingDemo";
 import SocialProof from "./HomePage/SocialProf";
 import AsistantModes from "./HomePage/AssistantModes";
+import CareerVault from "./HomePage/CareerVault";
 
 const Home = () => {
   return (
     <div className="p-10 text-center">
+     <CareerVault></CareerVault>
      <AsistantModes></AsistantModes>
      <PricingDemo></PricingDemo>
      <SocialProof></SocialProof>
