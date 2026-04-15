53aa8a1d3cd507396d40a0220fb7fd33c90a6588
Taoshif1 <taoshif2@gmail.com>
2026-04-15 13:49:04 +0600

    feat: implement AuthContext provider & PrivateRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./routes/router";
+import { AuthProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
 );
